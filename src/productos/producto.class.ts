e7659d3bdf9e3bea6d1388f7321fb87d1fef66de
Seba Gonzalez <ext_sebagonzalezm@falabella.cl>
2023-09-15 18:21:06 -0300

    doc: agrega api properties a producto

--- src/productos/producto.class.ts
+++ src/productos/producto.class.ts
@@ -1,10 +1,49 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class Producto {
+
+    @ApiProperty({
+        type: 'number',
+        title: 'Identificador del producto',  
+    })
     id: number;
+
+    @ApiProperty({
+        type: 'string',
+        title: 'Nombre del producto',  
+    })
     nombre: string;
+
+    @ApiProperty({
+        type: 'string',
+        title: 'Descripcion del producto',  
+    })
     descripcion: string;
+
+    @ApiProperty({
+        type: 'number',
+        title: 'Precio del producto',  
+    })
     precio: number;
+
+    @ApiProperty({
+        type: 'string',
+        title: 'url de la imagen del producto',  
+    })
     urlImagen: string;
+
+    @ApiProperty({
+        type: 'boolean',
+        title: 'Oferta',  
+        description: 'Indica si es un producto en oferta'
+    })
     oferta: boolean;
+
+    @ApiProperty({
+        type: 'boolean',
+        title: 'Producto recien llegado',
+        description: 'Indica si es un producto recien llegado'  
+    })
     recienLlegado: boolean;
 
     constructor(
