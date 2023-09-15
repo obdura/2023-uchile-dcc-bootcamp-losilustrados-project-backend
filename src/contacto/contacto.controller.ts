e9df29e18316b272cb63e25656525616dafeb375
Seba Gonzalez <ext_sebagonzalezm@falabella.cl>
2023-09-15 13:40:37 -0300

    doc: add descriptions to api properties

--- src/contacto/contacto.controller.ts
+++ src/contacto/contacto.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { ApiTags, ApiBody, ApiResponse } from '@nestjs/swagger';
+import { CreateContactMessageDto } from './dto/create-contact-message.dto';
+import { ContactResponseDto } from './dto/contact-response.dto';
 
 @ApiTags("Contacto")
 @Controller('contacto')
@@ -7,5 +9,21 @@ export class ContactoController {
     @Get()
     name(): string {
         return "<h1>Contacto</h1>"
-    } 
+    }
+
+    @ApiBody({ description: "Envia un mensaje de contacto.", type: CreateContactMessageDto })
+    @ApiResponse({ 
+        status: 200, 
+        description: 'Envío exitoso del mensaje de contacto.', 
+        type: ContactResponseDto 
+    })
+    @ApiResponse({ 
+        status: 400, 
+        description: 'Envio fallido del mensaje de contacto.', 
+        type: ContactResponseDto 
+    })
+    @Post()
+    sendMessage(@Body() createContactMessageDto: CreateContactMessageDto) {
+        return new ContactResponseDto('OK');
+    }
 }
