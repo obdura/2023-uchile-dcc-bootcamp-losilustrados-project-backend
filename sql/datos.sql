601b1c88a2d818fa50e4355cd5536ef0bc5f82b6
Sebastian Gonzalez Maturana <sigonzam@gmail.com>
2023-11-03 21:27:58 -0300

    Add sql scripts

---
+++ sql/datos.sql
@@ -0,0 +1,161 @@
+USE neotaller;
+
+INSERT INTO Artistas (nombre, apellido, url_imagen_perfil)
+VALUES 
+('Vicente', 'Van Gogh', 'url1'),
+('Juan', 'Picasso', 'url2'),
+('Dalí', 'Reyes', 'url3'),
+('David', 'Vinci', 'url4'),
+('Jaime', 'Monet', 'url5');
+
+INSERT INTO Categorias (nombre)
+VALUES
+('Poleras'),
+('Pantalones'),
+('Camisas'),
+('Polerones'),
+('Vestidos');
+
+INSERT INTO Clientes (email, password)
+VALUES
+('email1@gmail.com', 'clave123'),
+('email2@gmail.com', 'clave123'),
+('email3@gmail.com', 'clave123'),
+('email4@gmail.com', 'clave123'),
+('email5@gmail.com', 'clave123');
+
+INSERT INTO Cupones (descuento, fecha_desde_vigencia, fecha_hasta_vigencia)
+VALUES
+(10, '2023-10-31', '2023-11-01'),
+(11, '2023-10-31', '2023-11-02'),
+(12, '2023-10-31', '2023-11-03'),
+(13, '2023-10-31', '2023-11-04'),
+(14, '2023-10-31', '2023-11-05');
+
+INSERT INTO Direcciones (calle, numero, comuna, ciudad, region)
+VALUES 
+('calle 1', 123, 'La Florida', 'Santiago', 'RM'),
+('calle 2', 123, 'La Granja', 'Santiago', 'RM'),
+('calle 3', 123, 'La Pintana', 'Santiago', 'RM'),
+('calle 4', 123, 'Puente Alto', 'Santiago', 'RM'),
+('calle 5', 123, 'Las Condes', 'Santiago', 'RM');
+
+INSERT INTO Ilustraciones (id_artista, fecha_creacion)
+VALUES (1, '2023-10-31'), 
+(2, '2023-10-31'), 
+(3, '2023-10-31'), 
+(4, '2023-10-31'), 
+(5, '2023-10-31');
+
+INSERT INTO Proveedores (nombre)
+VALUES ('Importadora A'), ('Importadora B'), ('Importadora C'), ('Importadora D'), ('Importadora E');
+
+INSERT INTO Marcas (nombre, id_proveedor)
+VALUES
+('Adidas', 1),
+('Mossimo', 2),
+('Arrow', 3),
+('Gildan', 4),
+('Champions', 5);
+
+INSERT INTO Productos (precio, recien_llegado, id_categoria, id_marca, id_ilustracion, id_proveedor)
+VALUES
+(10000, True, 1, 5, 1, 1),
+(12000, True, 2, 4, 2, 2),
+(20000, True, 3, 3, 3, 2),
+(40000, False, 4, 2, 4, 4),
+(50000, False, 5, 1, 5, 1);
+
+INSERT INTO RepartoComuna (comuna)
+VALUES ('Santiago'), ('Estacion Central'), ('La Florida'), ('Pedro Aguirre Cerda'), ('Quilicura');
+
+INSERT INTO Telefonos (id_cliente, numero_telefono, es_celular)
+VALUES
+(1, '56975344443', True),
+(2, '56975344444', True),
+(3, '56275344423', False),
+(4, '56275344422', False),
+(2, '56975344421', True);
+
+INSERT INTO Pedidos (id_cliente, id_direccion, id_reparto_comuna, medio_pago)
+VALUES
+(1, 1, 1, 'Efectivo'),
+(1, 3, 2, 'Tarjeta Credito'),
+(3, 4, 3, 'Cheque'),
+(2, 2, 4, 'Tarjeta Debito'),
+(1, 1, 5, 'Webpay');
+
+INSERT INTO DetallePedidos (id_pedido, id_producto, cantidad)
+VALUES 
+(1, 1, 10),
+(1, 2, 3),
+(2, 4, 2),
+(2, 5, 3),
+(3, 5, 15);
+
+INSERT INTO Comprobantes (id_pedido, id_cliente)
+VALUES
+(1, 1),
+(2, 3),
+(3, 4),
+(4, 2),
+(5, 1);
+
+INSERT INTO Boletas (id_pedido, id_cliente, fecha_creacion)
+VALUES
+(1, 1, '2023-10-31'),
+(2, 3, '2023-10-30'),
+(3, 4, '2023-10-29'),
+(4, 2, '2023-10-28'),
+(5, 1, '2023-10-27');
+
+INSERT INTO DireccionesPorCliente (id_cliente, id_direccion)
+VALUES
+(1, 5),
+(2, 4),
+(3, 3),
+(4, 2),
+(5, 1);
+
+INSERT INTO Favoritos (id_producto, id_cliente, fecha_creacion)
+VALUES
+(1, 1, '2023-10-10'),
+(2, 2, '2023-10-11'),
+(2, 1, '2023-10-12'),
+(3, 2, '2023-10-10'),
+(5, 3, '2023-10-10');
+
+INSERT INTO Inventario (id_producto, talla, cantidad)
+VALUES
+(1, 'XL', 100),
+(2, 'M', 100),
+(1, 'S', 200),
+(3, 'L', 150),
+(4, 'L', 100);
+
+INSERT INTO Ofertas (id_producto, porcentaje_descuento)
+VALUES
+(1, 10),
+(2, 5),
+(3, 2),
+(4, 3),
+(5, 15);
+
+INSERT INTO OrdenTrabajo (fecha_creacion, id_pedido)
+VALUES
+('2023-10-30', 1),
+('2023-10-31', 2),
+('2023-11-01', 3),
+('2023-11-02', 4),
+('2023-11-02', 5);
+
+INSERT INTO Transacciones (id_cliente, id_pedido, fecha_transaccion, fecha_creacion)
+VALUES
+(1, 1, '2023-10-31', '2023-10-31'),
+(2, 2, '2023-10-30', '2023-10-30'),
+(2, 3, '2023-10-30', '2023-10-30'),
+(1, 4, '2023-10-29', '2023-10-29'),
+(1, 5, '2023-10-28', '2023-10-28');
+
+
+
---
+++ sql/estructura.sql
@@ -0,0 +1,202 @@
+DROP DATABASE neotaller;
+CREATE DATABASE neotaller;
+USE neotaller;
+
+CREATE TABLE `Productos` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `precio` integer,
+  `recien_llegado` bool,
+  `id_categoria` integer,
+  `id_marca` integer,
+  `id_ilustracion` integer,
+  `id_proveedor` integer
+);
+
+CREATE TABLE `Categorias` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `nombre` varchar(255)
+);
+
+CREATE TABLE `Ofertas` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `id_producto` integer,
+  `porcentaje_descuento` integer
+);
+
+CREATE TABLE `Inventario` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `id_producto` integer,
+  `talla` varchar(50),
+  `cantidad` integer
+);
+
+CREATE TABLE `Artistas` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `nombre` varchar(255),
+  `apellido` varchar(255),
+  `url_imagen_perfil` varchar(500)
+);
+
+CREATE TABLE `Ilustraciones` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `id_artista` integer,
+  `fecha_creacion` datetime
+);
+
+CREATE TABLE `Marcas` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `nombre` varchar(255),
+  `id_proveedor` integer
+);
+
+CREATE TABLE `Clientes` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `email` varchar(255),
+  `password` varchar(255)
+);
+
+CREATE TABLE `Pedidos` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `id_cliente` integer,
+  `id_direccion` integer,
+  `id_reparto_comuna` integer,
+  `fecha_creacion` datetime,
+  `medio_pago` varchar(255)
+);
+
+CREATE TABLE `DetallePedidos` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `id_pedido` integer,
+  `id_producto` integer,
+  `cantidad` integer
+);
+
+CREATE TABLE `Boletas` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `id_pedido` integer,
+  `id_cliente` integer,
+  `fecha_creacion` datetime
+);
+
+CREATE TABLE `Comprobantes` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `id_pedido` integer,
+  `id_cliente` integer
+);
+
+CREATE TABLE `Direcciones` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `calle` varchar(255),
+  `numero` varchar(10),
+  `numero_departamento` varchar(10),
+  `comuna` varchar(255),
+  `ciudad` varchar(255),
+  `provincia` varchar(255),
+  `region` varchar(255),
+  `codigo_postal` integer
+);
+
+CREATE TABLE `DireccionesPorCliente` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `id_cliente` integer,
+  `id_direccion` integer
+);
+
+CREATE TABLE `Favoritos` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `id_producto` integer,
+  `id_cliente` integer,
+  `fecha_creacion` datetime
+);
+
+CREATE TABLE `Telefonos` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `id_cliente` integer,
+  `numero_telefono` integer,
+  `es_celular` boolean
+);
+
+CREATE TABLE `Transacciones` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `id_cliente` integer,
+  `id_pedido` integer,
+  `fecha_transaccion` datetime,
+  `fecha_creacion` datetime
+);
+
+CREATE TABLE `Cupones` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `descuento` integer,
+  `fecha_desde_vigencia` datetime,
+  `fecha_hasta_vigencia` datetime,
+  `id_cliente` integer
+);
+
+CREATE TABLE `RepartoComuna` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `fecha_creacion` datetime,
+  `comuna` varchar(255)
+);
+
+CREATE TABLE `OrdenTrabajo` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `fecha_creacion` datetime,
+  `id_pedido` integer
+);
+
+CREATE TABLE `Proveedores` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `nombre` varchar(255)
+);
+
+ALTER TABLE `Pedidos` ADD FOREIGN KEY (`id_cliente`) REFERENCES `Clientes` (`id`);
+
+ALTER TABLE `DetallePedidos` ADD FOREIGN KEY (`id_pedido`) REFERENCES `Pedidos` (`id`);
+
+ALTER TABLE `Boletas` ADD FOREIGN KEY (`id_pedido`) REFERENCES `Pedidos` (`id`);
+
+ALTER TABLE `Comprobantes` ADD FOREIGN KEY (`id_pedido`) REFERENCES `Pedidos` (`id`);
+
+ALTER TABLE `DireccionesPorCliente` ADD FOREIGN KEY (`id_cliente`) REFERENCES `Clientes` (`id`);
+
+ALTER TABLE `DireccionesPorCliente` ADD FOREIGN KEY (`id_direccion`) REFERENCES `Direcciones` (`id`);
+
+ALTER TABLE `Favoritos` ADD FOREIGN KEY (`id_producto`) REFERENCES `Productos` (`id`);
+
+ALTER TABLE `Favoritos` ADD FOREIGN KEY (`id_cliente`) REFERENCES `Clientes` (`id`);
+
+ALTER TABLE `Boletas` ADD FOREIGN KEY (`id_cliente`) REFERENCES `Clientes` (`id`);
+
+ALTER TABLE `Telefonos` ADD FOREIGN KEY (`id_cliente`) REFERENCES `Clientes` (`id`);
+
+ALTER TABLE `Comprobantes` ADD FOREIGN KEY (`id_cliente`) REFERENCES `Clientes` (`id`);
+
+ALTER TABLE `Transacciones` ADD FOREIGN KEY (`id_cliente`) REFERENCES `Clientes` (`id`);
+
+ALTER TABLE `Cupones` ADD FOREIGN KEY (`id_cliente`) REFERENCES `Clientes` (`id`);
+
+ALTER TABLE `Pedidos` ADD FOREIGN KEY (`id_direccion`) REFERENCES `Direcciones` (`id`);
+
+ALTER TABLE `Pedidos` ADD FOREIGN KEY (`id_reparto_comuna`) REFERENCES `RepartoComuna` (`id`);
+
+ALTER TABLE `OrdenTrabajo` ADD FOREIGN KEY (`id_pedido`) REFERENCES `Pedidos` (`id`);
+
+ALTER TABLE `Transacciones` ADD FOREIGN KEY (`id_pedido`) REFERENCES `Pedidos` (`id`);
+
+ALTER TABLE `Ilustraciones` ADD FOREIGN KEY (`id_artista`) REFERENCES `Artistas` (`id`);
+
+ALTER TABLE `Inventario` ADD FOREIGN KEY (`id_producto`) REFERENCES `Productos` (`id`);
+
+ALTER TABLE `Productos` ADD FOREIGN KEY (`id_categoria`) REFERENCES `Categorias` (`id`);
+
+ALTER TABLE `Productos` ADD FOREIGN KEY (`id_ilustracion`) REFERENCES `Ilustraciones` (`id`);
+
+ALTER TABLE `Productos` ADD FOREIGN KEY (`id_proveedor`) REFERENCES `Proveedores` (`id`);
+
+ALTER TABLE `DetallePedidos` ADD FOREIGN KEY (`id_producto`) REFERENCES `Productos` (`id`);
+
+ALTER TABLE `Productos` ADD FOREIGN KEY (`id_marca`) REFERENCES `Marcas` (`id`);
+
+ALTER TABLE `Ofertas` ADD FOREIGN KEY (`id_producto`) REFERENCES `Productos` (`id`);
+
+ALTER TABLE `Marcas` ADD FOREIGN KEY (`id_proveedor`) REFERENCES `Proveedores` (`id`);
