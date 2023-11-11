af29d9d50766f4c1a4f90d69e5c56d1793531153
Sebastian Gonzalez Maturana <sigonzam@gmail.com>
2023-11-11 14:16:54 -0300

    New dtos

---
+++ src/dtos/producto.dto.ts
@@ -0,0 +1,29 @@
+import { ApiProperty } from "@nestjs/swagger";
+
+
+export class ProductoDto {
+
+    @ApiProperty({ description: "Id del producto" })
+    id: number;
+
+    @ApiProperty({ description: "Nombre del producto" })
+    nombre: string;
+
+    @ApiProperty({ description: "Descripcion del producto" })
+    descripcion: string;
+    
+    @ApiProperty({ description: "Id de la categoría del producto" })
+    idCategoria: number;
+    
+    @ApiProperty({ description: "Id de la ilustración del producto" })
+    idIlustracion: number;
+    
+    @ApiProperty({ description: "Id de la marca del producto" })
+    idMarca: number;
+    
+    @ApiProperty({ description: "Id del proveedor del producto" })
+    idProveedor: number;
+    
+    @ApiProperty({ description: "Precio del producto" })
+    precio: number;
+}
