80820c372a4a3fd703612dfa900a8b240fb97c34
Sebastian Gonzalez Maturana <sigonzam@gmail.com>
2023-11-16 21:21:23 -0300

    create find all endpoint for clientes

--- src/controllers/clientes.controller.ts
+++ src/controllers/clientes.controller.ts
@@ -1,8 +1,9 @@
-import { BadRequestException, Body, Controller, Patch, Post } from "@nestjs/common";
+import { BadRequestException, Body, Controller, DefaultValuePipe, Get, ParseIntPipe, Patch, Post, Query } from "@nestjs/common";
 import { CreateClienteDto } from "src/dtos/create-cliente.dto";
 import { ClienteDto } from "src/dtos/cliente.dto";
 import { ClientesService } from "src/services/clientes.service";
-import { ApiBody, ApiOkResponse, ApiTags } from "@nestjs/swagger";
+import { ApiBody, ApiOkResponse, ApiQuery, ApiTags } from "@nestjs/swagger";
+import { Cliente } from "src/entidades/cliente.entity";
 
 @ApiTags("Usuarios")
 @Controller("clientes")
@@ -21,6 +22,22 @@ export class ClientesController {
         try {
             const cliente: ClienteDto = await this.clienteService.createCliente(createClienteDto);
             return cliente;
+        } catch (error) {
+            throw new BadRequestException(error.message);
+        }
+    }
+
+    @Get()
+    @ApiQuery({ name: 'page', required: false, type: Number, description: 'La página del listado de clientes.'})
+    @ApiQuery({ name: 'limit', required: false, type: Number, description: 'El número de clientes a mostrar por página.'})
+    @ApiOkResponse({ description: 'Los clientes encontrados.', type: ClienteDto, isArray: true })
+    async findAllClientes(
+        @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+        @Query('limit', new DefaultValuePipe(5), ParseIntPipe) limit: number
+    ) {
+        try {
+            const resultados: ClienteDto[] = await this.clienteService.findAll(page, limit);
+            return resultados;
         } catch (error) {
             throw new BadRequestException(error.message);
         }
