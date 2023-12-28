b3d6a591bfbc6630402c0de3324c38762a85984b
Seba Gonzalez <sigonzam@gmailcom>
2023-12-28 18:41:06 -0300

    Add autorization for carrito post

--- src/controllers/carrito.controller.ts
+++ src/controllers/carrito.controller.ts
@@ -1,10 +1,18 @@
-import { BadRequestException, Body, Controller, Post } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
+import { BadRequestException, Body, Controller, Post, UseGuards } from "@nestjs/common";
+import { ApiBearerAuth, ApiHeader, ApiTags } from "@nestjs/swagger";
+import { Roles } from "src/decorator/roles.decorator";
 import { CreateCarritoDto } from "src/dtos/create-carrito.dto";
+import { Role } from "src/enum/role.enum";
+import { AuthenticationGuard } from "src/guards/authentication.guard";
+import { RolesGuard } from "src/guards/roles.guard";
 import { CarritoService } from "src/services/carrito.service";
 
 @ApiTags("Carrito")
 @Controller("carrito")
+@ApiBearerAuth('general')
+@Roles(Role.Cliente)
+@UseGuards(AuthenticationGuard, RolesGuard)
+@ApiHeader({ name: "Authentication", description: "Token de autenticación", required: true })
 export class CarritoController {
     
     constructor(
